5a1356f2ac729b94c7fe48a9a8e8909734085090
CodeNinjasEtobicoke <107214218+CodeNinjasEtobicoke@users.noreply.github.com>
2025-05-15 18:08:58 -0400

    5ete
    t4tteet4et

---
+++ CyberFu-Liam/Assets/Scripts/EnemyControls.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControls : MonoBehaviour
+{
+    [SerializeField]
+    private bool isFollowingTarget;
+
+    public float speed = 2f;
+    public float attackingDistance = 0.6f;
+
+    private Animator animatorEnemy;
+    private Rigidbody rigidbodyEnemy;
+    private Transform target;
+
+    
+
+    private float currentAttakingTime = 0f;
+    private float maxAttackingTime = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animatorEnemy = GetComponent<Animator>();
+        rigidbodyEnemy = GetComponent<Rigidbody>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.position);
+
+        isFollowingTarget = Vector3.Distance(transform.position, target.position) >= attackingDistance;
+
+        if (isFollowingTarget)
+        {
+            rigidbodyEnemy.velocity = transform.forward * speed;
+        }
+
+    }
+}
